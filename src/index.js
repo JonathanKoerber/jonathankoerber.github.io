c89666eb9b067f39f12a2f623775adfda04bbc49
JonathanKoerber <jonathankoerber@gmail.com>
2023-10-14 11:38:43 -0700

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App";
-
-
-createRoot(document.getElementById('root')).render(<App />);
